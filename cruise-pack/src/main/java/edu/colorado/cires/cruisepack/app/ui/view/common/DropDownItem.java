99b4fec7ab00bcf0dd14f631bd5ff7ed3830b87e
Chris Slater <6004777+electricsam@users.noreply.github.com>
2024-02-06 13:10:20 -0700

    dropdown value now displays

--- cruise-pack/src/main/java/edu/colorado/cires/cruisepack/app/ui/view/common/DropDownItem.java
+++ cruise-pack/src/main/java/edu/colorado/cires/cruisepack/app/ui/view/common/DropDownItem.java
@@ -1,5 +1,7 @@
 package edu.colorado.cires.cruisepack.app.ui.view.common;
 
+import java.util.Objects;
+
 public class DropDownItem {
 
   private final String id;
@@ -15,6 +17,28 @@ public class DropDownItem {
   }
 
   public String getValue() {
+    return value;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    DropDownItem that = (DropDownItem) o;
+    return Objects.equals(id, that.id) && Objects.equals(value, that.value);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, value);
+  }
+
+  @Override
+  public String toString() {
     return value;
   }
 }
