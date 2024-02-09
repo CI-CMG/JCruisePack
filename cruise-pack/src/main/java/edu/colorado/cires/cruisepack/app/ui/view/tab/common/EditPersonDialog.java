ebba4d5470fddcdfa9658d7c706a853c78b71f38
paytoncain <payton.cain@colorado.edu>
2024-02-09 11:24:56 -0700

    Update create/edit forms layout

--- cruise-pack/src/main/java/edu/colorado/cires/cruisepack/app/ui/view/tab/common/EditPersonDialog.java
+++ cruise-pack/src/main/java/edu/colorado/cires/cruisepack/app/ui/view/tab/common/EditPersonDialog.java
@@ -12,11 +12,11 @@ import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 import org.springframework.beans.factory.BeanFactory;
 import edu.colorado.cires.cruisepack.app.ui.view.MainFrame;
+import edu.colorado.cires.cruisepack.app.ui.view.common.StatefulRadioButton;
 import edu.colorado.cires.cruisepack.app.ui.view.tab.peopletab.PeopleList;
 
 public class EditPersonDialog extends JComponent {
@@ -30,49 +30,159 @@ public class EditPersonDialog extends JComponent {
 
     private void init(BeanFactory beanFactory, PeopleList peopleList) {
         JDialog dialog = new JDialog(beanFactory.getBean(MainFrame.class), PEOPLE_EDITOR_HEADER, true);
-      dialog.setLayout(new BorderLayout(5, 5));
+        dialog.setLayout(new BorderLayout(0, 0));
 
-      JPanel panel = new JPanel();
-      panel.setLayout(new GridBagLayout());
-      panel.add(new JComboBox<>(peopleList.getPeopleList()), configureLayout(0, 0, c -> c.gridwidth = GridBagConstraints.REMAINDER));
-      panel.add(new JLabel("Name (First Last)"), configureLayout(0, 1, c -> c.gridwidth = GridBagConstraints.REMAINDER));
-      panel.add(new JTextField(), configureLayout(0, 2, c -> c.gridwidth = GridBagConstraints.REMAINDER));
-      panel.add(new JLabel("Position"), configureLayout(0, 3));
-      panel.add(new JLabel("Organization"), configureLayout(1, 3, c -> c.gridwidth = GridBagConstraints.REMAINDER));
-      panel.add(new JTextField(), configureLayout(0, 4));
-      panel.add(new JTextField(), configureLayout(1, 4, c -> c.gridwidth = GridBagConstraints.REMAINDER));
-      panel.add(new JLabel("Street"), configureLayout(0, 5, c -> c.gridwidth = GridBagConstraints.REMAINDER));
-      panel.add(new JTextField(), configureLayout(0, 6, c -> c.gridwidth = GridBagConstraints.REMAINDER));
-      panel.add(new JLabel("City"), configureLayout(0, 7));
-      panel.add(new JLabel("State/Administrative Area"), configureLayout(1, 7, c -> c.gridwidth = GridBagConstraints.REMAINDER));
-      panel.add(new JTextField(), configureLayout(0, 8));
-      panel.add(new JTextField(), configureLayout(1, 8, c -> c.gridwidth = GridBagConstraints.REMAINDER));
-      panel.add(new JLabel("Postal Code"), configureLayout(0, 9));
-      panel.add(new JLabel("Country"), configureLayout(1, 9));
-      panel.add(new JLabel("Phone"), configureLayout(2, 9, 3, c -> c.gridwidth = GridBagConstraints.REMAINDER));
-      panel.add(new JTextField(), configureLayout(0, 10));
-      panel.add(new JTextField("USA"), configureLayout(1, 10));
-      panel.add(new JTextField(), configureLayout(2, 10, 3, c -> c.gridwidth = GridBagConstraints.REMAINDER));
-      panel.add(new JLabel("Email"), configureLayout(0, 11));
-      panel.add(new JLabel("ORCID ID"), configureLayout(1, 11, c -> c.gridwidth = GridBagConstraints.REMAINDER));
-      panel.add(new JTextField(), configureLayout(0, 12));
-      panel.add(new JTextField(), configureLayout(1, 12, c -> c.gridwidth = GridBagConstraints.REMAINDER));
-      panel.add(new JLabel("UUID (automatically generated when you click save)"), configureLayout(0, 13, c -> c.gridwidth = GridBagConstraints.REMAINDER));
-      JTextField uuidTextField = new JTextField();
-      uuidTextField.setEditable(false);
-      panel.add(uuidTextField, configureLayout(0, 14, c -> c.gridwidth = GridBagConstraints.REMAINDER));
+        JPanel formPanel = new JPanel();
+        formPanel.setLayout(new GridBagLayout());
+        formPanel.add(new JComboBox<>(peopleList.getPeopleList()), configureLayout(0, 0, c -> c.gridwidth = GridBagConstraints.REMAINDER));
+        
+        JPanel namePanel = new JPanel();
+        namePanel.setLayout(new GridBagLayout());
+        namePanel.add(new JLabel("Name (First Last)"), configureLayout(0, 0, c -> {
+            c.weightx = 100;
+        }));
+        namePanel.add(new JTextField(), configureLayout(0, 1, c -> {
+            c.weightx = 100;
+        }));
 
-      JPanel subPanel = new JPanel();
-      subPanel.setLayout(new GridBagLayout());
-      subPanel.add(new JLabel("Display in pull-down lists:"), configureLayout(0, 0));
-      subPanel.add(new JRadioButton("Yes"), configureLayout(1, 0));
-      subPanel.add(new JRadioButton("No"), configureLayout(2, 0));
-      subPanel.add(new JButton("Clear"), configureLayout(3, 0));
-      subPanel.add(new JButton("Save"), configureLayout(4, 0, c -> c.gridwidth = GridBagConstraints.REMAINDER));
-      dialog.add(panel, BorderLayout.PAGE_START);
-      dialog.add(subPanel, BorderLayout.PAGE_END);
-      dialog.pack();
-      dialog.setVisible(true);
+        formPanel.add(namePanel, configureLayout(0, 1, c -> {
+            c.weighty = 100;
+            c.gridwidth = GridBagConstraints.REMAINDER;
+        }));
+        
+        JPanel positionOrgPanel = new JPanel();
+        positionOrgPanel.setLayout(new GridBagLayout());
+        positionOrgPanel.add(new JLabel("Position"), configureLayout(0, 0, c -> {
+            c.weightx = 50;
+        }));
+        positionOrgPanel.add(new JTextField(), configureLayout(0, 1, c -> {
+            c.weightx = 50;
+        }));
+        positionOrgPanel.add(new JLabel("Organization"), configureLayout(1, 0, c -> {
+            c.weightx = 50;
+        }));
+        positionOrgPanel.add(new JTextField(), configureLayout(1, 1, c -> {
+            c.weightx = 50;
+        }));
+
+        formPanel.add(positionOrgPanel, configureLayout(0, 2, c -> {
+            c.weighty = 100;
+            c.gridwidth = GridBagConstraints.REMAINDER;
+        }));
+        
+        JPanel streetPanel = new JPanel();
+        streetPanel.setLayout(new GridBagLayout());
+        streetPanel.add(new JLabel("Street"), configureLayout(0, 0, c -> {
+            c.weightx = 100;
+        }));
+        streetPanel.add(new JTextField(), configureLayout(0, 1, c -> {
+            c.weightx = 100;
+        }));
+
+        formPanel.add(streetPanel, configureLayout(0, 3, c -> {
+            c.weighty = 100;
+            c.gridwidth = GridBagConstraints.REMAINDER;
+        }));
+        
+        JPanel cityStatePanel = new JPanel();
+        cityStatePanel.setLayout(new GridBagLayout());
+        cityStatePanel.add(new JLabel("City"), configureLayout(0, 0, c -> {
+            c.weightx = 50;
+        }));
+        cityStatePanel.add(new JTextField(), configureLayout(0, 1, c -> {
+            c.weightx = 50;
+        }));
+        cityStatePanel.add(new JLabel("State/Administrative Area"), configureLayout(1, 0, c -> {
+            c.weightx = 50;
+        }));
+        cityStatePanel.add(new JTextField(), configureLayout(1, 1, c -> {
+            c.weightx = 50;
+        }));
+
+        formPanel.add(cityStatePanel, configureLayout(0, 4, c -> {
+            c.weighty = 100;
+            c.gridwidth = GridBagConstraints.REMAINDER;
+        }));
+        
+        JPanel postalCountryPhonePanel = new JPanel();
+        postalCountryPhonePanel.setLayout(new GridBagLayout());
+        postalCountryPhonePanel.add(new JLabel("Postal Code"), configureLayout(0, 0, c -> {
+            c.weightx = 20;
+        }));
+        postalCountryPhonePanel.add(new JTextField(), configureLayout(0, 1, c -> {
+            c.weightx = 20;
+        }));
+        postalCountryPhonePanel.add(new JLabel("Country"), configureLayout(1, 0, c -> {
+            c.weightx = 30;
+        }));
+        postalCountryPhonePanel.add(new JTextField("USA"), configureLayout(1, 1, c -> {
+            c.weightx = 30;
+        }));
+        postalCountryPhonePanel.add(new JLabel("Phone"), configureLayout(2, 0, c -> {
+            c.weightx = 50;
+        }));
+        postalCountryPhonePanel.add(new JTextField(), configureLayout(2, 1, c -> {
+            c.weightx = 50;
+        }));
+
+        formPanel.add(postalCountryPhonePanel, configureLayout(0, 5, c -> {
+            c.weighty = 100;
+            c.gridwidth = GridBagConstraints.REMAINDER;
+        }));
+        
+        JPanel emailOrcidIDPanel = new JPanel();
+        emailOrcidIDPanel.setLayout(new GridBagLayout());
+        emailOrcidIDPanel.add(new JLabel("Email"), configureLayout(0, 0, c -> {
+            c.weightx = 50;
+        }));
+        emailOrcidIDPanel.add(new JTextField(), configureLayout(0, 1, c -> {
+            c.weightx = 50;
+        }));
+        emailOrcidIDPanel.add(new JLabel("ORCID ID"), configureLayout(1, 0, c -> {
+            c.weightx = 50;
+        }));
+        emailOrcidIDPanel.add(new JTextField(), configureLayout(1, 1, c -> {
+            c.weightx = 50;
+        }));
+        
+        formPanel.add(emailOrcidIDPanel, configureLayout(0, 6, c -> {
+            c.weighty = 100;
+            c.gridwidth = GridBagConstraints.REMAINDER;
+        }));
+        
+        JPanel uuidPanel = new JPanel();
+        uuidPanel.setLayout(new GridBagLayout());
+        uuidPanel.add(new JLabel("UUID (automatically generated when you click save)"), configureLayout(0, 0, c -> c.gridwidth = GridBagConstraints.REMAINDER));
+        JTextField uuidTextField = new JTextField();
+        uuidTextField.setEditable(false);
+        uuidPanel.add(uuidTextField, configureLayout(0, 1, c -> c.gridwidth = GridBagConstraints.REMAINDER));
+
+        formPanel.add(uuidPanel, configureLayout(0, 7, c -> {
+            c.weighty = 100;
+            c.gridwidth = GridBagConstraints.REMAINDER;
+        }));
+
+        JPanel clearSaveButtonsPannel = new JPanel();
+        clearSaveButtonsPannel.setLayout(new GridBagLayout());
+        clearSaveButtonsPannel.add(new JButton("Clear"), configureLayout(0, 0, c -> {
+            c.weightx = 0;
+        }));
+        clearSaveButtonsPannel.add(new JButton("Save"), configureLayout(1, 0, c -> {
+            c.weightx = 0;
+        }));
+
+        JPanel footerPannel = new JPanel();
+        footerPannel.setLayout(new BorderLayout());
+        footerPannel.add(new StatefulRadioButton("Display in pull-down lists:"), BorderLayout.LINE_START);
+        footerPannel.add(clearSaveButtonsPannel, BorderLayout.LINE_END);
+
+        formPanel.add(footerPannel, configureLayout(0, 8, c -> {
+            c.weighty = 0;
+            c.gridwidth = GridBagConstraints.REMAINDER;
+        }));
+        dialog.add(formPanel);
+        dialog.pack();
+        dialog.setVisible(true);
     }
 
 
