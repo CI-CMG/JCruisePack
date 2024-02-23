5d8da551a88a7464856696ff1a96e1edaca2eeb8
paytoncain <payton.cain@colorado.edu>
2024-02-23 13:39:45 -0700

    Remove unused imports

--- cruise-pack/src/main/java/edu/colorado/cires/cruisepack/app/service/DatasetPacker.java
+++ cruise-pack/src/main/java/edu/colorado/cires/cruisepack/app/service/DatasetPacker.java
@@ -1,26 +1,5 @@
 package edu.colorado.cires.cruisepack.app.service;
 
-import static edu.colorado.cires.cruisepack.app.service.CruisePackFileUtils.copy;
-import static edu.colorado.cires.cruisepack.app.service.CruisePackFileUtils.filterHidden;
-import static edu.colorado.cires.cruisepack.app.service.CruisePackFileUtils.filterTimeSize;
-import static edu.colorado.cires.cruisepack.app.service.CruisePackFileUtils.mkDir;
-
-import edu.colorado.cires.cruisepack.app.config.ServiceProperties;
-import edu.colorado.cires.cruisepack.app.service.metadata.CruiseMetadata;
-import gov.loc.repository.bagit.creator.BagCreator;
-import gov.loc.repository.bagit.hash.StandardSupportedAlgorithms;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-import java.util.List;
-import org.apache.commons.io.FilenameUtils;
-
 public class DatasetPacker {
 
 
