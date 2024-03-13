843bfe965b3d0d756631601905e3c4ab0aaa51b7
paytoncain <payton.cain@colorado.edu>
2024-03-13 16:39:30 -0600

    Add checksum/manifest utilities to CruisePackFileUtils

--- src/main/java/edu/colorado/cires/cruisepack/app/service/CruisePackFileUtils.java
+++ src/main/java/edu/colorado/cires/cruisepack/app/service/CruisePackFileUtils.java
@@ -1,8 +1,16 @@
 package edu.colorado.cires.cruisepack.app.service;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.FileUtils;
 
 public final class CruisePackFileUtils {
@@ -32,12 +40,80 @@ public final class CruisePackFileUtils {
       throw new IllegalStateException("Unable to create directory " + dir, e);
     }
   }
+  
+  public static void cleanDir(Path dir) {
+    try {
+      FileUtils.cleanDirectory(dir.toFile());
+    } catch (IOException e) {
+      throw new IllegalStateException("Unable to clean directory " + dir, e);
+    }
+  }
 
   public static void copy(Path source, Path target) {
     try {
       FileUtils.copyFile(source.toFile(), target.toFile(), true);
     } catch (IOException e) {
       throw new IllegalStateException("Unable to copy file " + source + " to " + target, e);
+    }
+  }
+  
+  public static void concatManifests(Path childManifest, Path mainBagPath, FileWriter parentManifestWriter) {
+    try (
+        FileReader reader = new FileReader(childManifest.toFile(), StandardCharsets.UTF_8);
+        BufferedReader bufferedReader = new BufferedReader(reader)
+    ) {
+      String line = bufferedReader.readLine();
+      while (line != null) {
+        String[] lineParts = line.split(" {2}");
+        Path path = mainBagPath.relativize(childManifest.getParent()).resolve(lineParts[1]);
+        parentManifestWriter.write(String.format(
+            "%s  %s\n",
+            lineParts[0], path
+        ));
+        line = bufferedReader.readLine();
+      }
+    } catch (IOException e) {
+      throw new IllegalStateException(String.format(
+          "Failed to append to manifest from %s",
+          childManifest
+      ), e);
+    }
+  }
+  
+  public static void appendToManifest(Path path, Path mainBagPath, FileWriter manifestWriter) {
+    try (Stream<Path> paths = Files.walk(path)) {
+      paths.filter(p -> p.toFile().isFile())
+          .forEach(p -> {
+            try {
+              manifestWriter.write(
+                  String.format(
+                      "%s  %s\n",
+                      computeChecksum(p), mainBagPath.relativize(p)
+                  )
+              );
+            } catch (IOException e) {
+              throw new IllegalStateException(String.format(
+                  "Failed to append %s to manifest",
+                  p
+              ), e);
+            }
+          });
+    } catch (IOException e) {
+      throw new IllegalStateException(String.format(
+          "Failed to append %s to manifest",
+          path
+      ), e);
+    }
+  }
+  
+  public static String computeChecksum(Path path) {
+    try (InputStream inputStream = new FileInputStream(path.toFile())) {
+      return DigestUtils.sha256Hex(inputStream);
+    } catch (IOException e) {
+      throw new IllegalStateException(String.format(
+          "Failed to compute checksum for %s",
+          path
+      ), e);
     }
   }
 
