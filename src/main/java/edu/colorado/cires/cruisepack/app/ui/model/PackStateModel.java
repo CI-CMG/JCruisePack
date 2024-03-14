7216dfeb12bcb087523885980f4b7f158cd4891e
paytoncain <payton.cain@colorado.edu>
2024-03-14 13:14:55 -0600

    Remove thread from pack state model, add in progress boolean field

--- src/main/java/edu/colorado/cires/cruisepack/app/ui/model/PackStateModel.java
+++ src/main/java/edu/colorado/cires/cruisepack/app/ui/model/PackStateModel.java
@@ -7,19 +7,21 @@ import org.springframework.stereotype.Component;
 public class PackStateModel extends PropertyChangeModel {
   
   private int progress;
-  private Thread thread;
+  private boolean processing;
 
   public void setProgress(int progress) {
     setIfChanged(Events.UPDATE_PROGRESS, progress, () -> this.progress, (p) -> this.progress = p);
   }
 
-  public void setThread(Thread thread) {
-    this.thread = thread;
+  public int getProgress() {
+    return progress;
   }
-  
-  public void stopThread() {
-    if (thread != null) {
-      thread.interrupt();
-    }
+
+  public boolean isProcessing() {
+    return processing;
+  }
+
+  public void setProcessing(boolean processing) {
+    this.processing = processing;
   }
 }
