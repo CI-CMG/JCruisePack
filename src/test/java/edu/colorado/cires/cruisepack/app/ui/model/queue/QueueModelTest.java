b42f8ddada741df23d45ab9412ffe02334ad0857
paytoncain <payton.cain@colorado.edu>
2024-03-24 21:52:52 -0600

    Add queue model test

---
+++ src/test/java/edu/colorado/cires/cruisepack/app/ui/model/queue/QueueModelTest.java
@@ -0,0 +1,192 @@
+package edu.colorado.cires.cruisepack.app.ui.model.queue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import edu.colorado.cires.cruisepack.app.service.PackJob;
+import edu.colorado.cires.cruisepack.app.ui.model.PackStateModel;
+import edu.colorado.cires.cruisepack.app.ui.model.PropertyChangeModelTest;
+import edu.colorado.cires.cruisepack.app.ui.view.queue.PackJobPanel;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+
+class QueueModelTest extends PropertyChangeModelTest<QueueModel> {
+
+  @Override
+  protected QueueModel createModel() {
+    return new QueueModel();
+  }
+  
+  private PackJobPanel createPanel(String packageId) {
+    return new PackJobPanel(
+        PackJob.builder()
+            .setPackageId(packageId)
+            .setCruiseId(UUID.randomUUID().toString())
+            .build()
+    );
+  }
+
+  @Test
+  void addToQueue() {
+    PackJobPanel panel1 = createPanel(UUID.randomUUID().toString());
+    PackJobPanel panel2 = createPanel(UUID.randomUUID().toString());
+
+    List<PackJobPanel> panels = List.of(
+        panel1, panel2
+    );
+    panels.forEach(model::addToQueue);
+    
+    assertChangeEvents(
+        QueueModel.ADD_TO_QUEUE,
+        panels.stream()
+            .map(p -> (PackJobPanel) null)
+            .toList(),
+        panels,
+        (v) -> v
+    );
+    assertEquals(
+        panels.stream()
+            .map(PackJobPanel::getPackJob)
+            .toList(),
+        model.getQueue()
+    );
+  }
+
+  @Test
+  void removeFromQueue() {
+    PackJobPanel panel1 = createPanel(UUID.randomUUID().toString());
+    PackJobPanel panel2 = createPanel(UUID.randomUUID().toString());
+
+    List<PackJobPanel> panels = List.of(
+        panel1, panel2
+    );
+    panels.forEach(model::addToQueue);
+    panels.forEach(model::removeFromQueue);
+    
+    assertChangeEvents(
+        QueueModel.REMOVE_FROM_QUEUE,
+        panels,
+        panels.stream()
+            .map(p -> (PackJobPanel) null)
+            .toList(),
+        (v) -> v
+    );
+    assertEquals(Collections.emptyList(), model.getQueue());
+  }
+
+  @Test
+  void clearQueue() {
+    PackJobPanel panel1 = createPanel(UUID.randomUUID().toString());
+    PackJobPanel panel2 = createPanel(UUID.randomUUID().toString());
+
+    List<PackJobPanel> panels = List.of(
+        panel1, panel2
+    );
+    panels.forEach(model::addToQueue);
+    model.clearQueue();
+    
+    assertChangeEvent(
+        QueueModel.CLEAR_QUEUE,
+        panels.stream()
+            .map(PackJobPanel::getPackJob)
+            .toList(),
+        Collections.emptyList()
+    );
+    assertEquals(Collections.emptyList(), model.getQueue());
+  }
+
+  @Test
+  void updatePackageQueueButton() {
+    model.updatePackageQueueButton(true);
+    model.updatePackageQueueButton(false);
+    
+    assertChangeEvents(
+        QueueModel.UPDATE_PACKAGE_QUEUE_BUTTON,
+        List.of(
+            false, true
+        ),
+        List.of(
+            true, false
+        ),
+        (v) -> v
+    );
+  }
+
+  @Test
+  void updateClearQueueButton() {
+    model.updateClearQueueButton(true);
+    model.updateClearQueueButton(false);
+    
+    assertChangeEvents(
+        QueueModel.UPDATE_CLEAR_QUEUE_BUTTON,
+        List.of(
+            false, true
+        ),
+        List.of(
+            true, false
+        ),
+        (v) -> v
+    );
+  }
+
+  @Test
+  void updatePackageButton() {
+    String processId = UUID.randomUUID().toString();
+    model.updatePackageButton(true, processId);
+    model.updatePackageButton(false, processId);
+    
+    assertChangeEvents(
+        String.format(
+            "UPDATE_PACKAGE_BUTTON_%s", processId
+        ),
+        List.of(
+            false, true
+        ),
+        List.of(
+            true, false
+        ),
+        (v) -> v
+    );
+  }
+
+  @Test
+  void updateRemoveButton() {
+    String processId = UUID.randomUUID().toString();
+    model.updateRemoveButton(true, processId);
+    model.updateRemoveButton(false, processId);
+    
+    assertChangeEvents(
+        String.format(
+            "UPDATE_REMOVE_BUTTON_%s", processId
+        ),
+        List.of(
+            false, true
+        ),
+        List.of(
+            true, false
+        ),
+        (v) -> v
+    );
+  }
+
+  @Test
+  void updateStopButton() {
+    String processId = UUID.randomUUID().toString();
+    model.updateStopButton(true, processId);
+    model.updateStopButton(false, processId);
+    
+    assertChangeEvents(
+        String.format(
+            "UPDATE_STOP_BUTTON_%s", processId
+        ),
+        List.of(
+            false, true
+        ),
+        List.of(
+            true, false
+        ),
+        (v) -> v
+    );
+  }
+}
