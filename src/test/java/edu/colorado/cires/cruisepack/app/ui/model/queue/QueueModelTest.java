01f0c2970d54d3282956646890f990900c972f77
paytoncain <payton.cain@colorado.edu>
2024-03-26 11:52:00 -0600

    Update remove stop all button test from queue model tests

--- src/test/java/edu/colorado/cires/cruisepack/app/ui/model/queue/QueueModelTest.java
+++ src/test/java/edu/colorado/cires/cruisepack/app/ui/model/queue/QueueModelTest.java
@@ -134,20 +134,4 @@ class QueueModelTest extends PropertyChangeModelTest<QueueModel> {
         (v) -> v
     );
   }
-  
-  @Test
-  void updateStopAllButton() {
-    model.updateStopAllButton(true);
-    model.updateStopAllButton(false);
-    assertChangeEvents(
-        QueueModel.UPDATE_STOP_ALL_BUTTON,
-        List.of(
-            false, true
-        ),
-        List.of(
-            true, false
-        ),
-        (v) -> v
-    );
-  }
 }
