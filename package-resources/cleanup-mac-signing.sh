0cb1a2289bb2795644111a7d310ec22ba680652a
Chris Slater <6004777+electricsam@users.noreply.github.com>
2024-04-12 07:11:24 -0600

    simplify mac signing

--- package-resources/cleanup-mac-signing.sh
+++ package-resources/cleanup-mac-signing.sh
@@ -5,9 +5,6 @@ set -ex
 # Delete temporary keychain
 security delete-keychain "$MY_KEYCHAIN"
 
-# default again user login keychain
-security list-keychains -d user -s login.keychain
-
 #remove files
 rm app_sign.p12.txt
 rm app_sign.p12
--- package-resources/setup-mac-signing.sh
+++ package-resources/setup-mac-signing.sh
@@ -8,14 +8,8 @@ echo "$INSTALL_CERT_BASE64" > install_sign.p12.txt
 base64 -d -i app_sign.p12.txt -o app_sign.p12
 base64 -d -i install_sign.p12.txt -o install_sign.p12
 
-# default again user login keychain
-security list-keychains -d user -s login.keychain
-
 # Create temp keychain
 security create-keychain -p "$MY_KEYCHAIN_PASSWORD" "$MY_KEYCHAIN"
-
-# Append temp keychain to the user domain
-security list-keychains -d user -s "$MY_KEYCHAIN" $(security list-keychains -d user | sed s/\"//g)
 
 # Remove relock timeout
 security set-keychain-settings "$MY_KEYCHAIN"
@@ -26,6 +20,3 @@ security unlock-keychain -p "$MY_KEYCHAIN_PASSWORD" "$MY_KEYCHAIN"
 # Add certificate to keychain
 security import app_sign.p12 -k "$MY_KEYCHAIN" -P "$APP_CERT_PASSWORD" -A -T "/usr/bin/codesign" -T "/usr/bin/productsign"
 security import install_sign.p12 -k "$MY_KEYCHAIN" -P "$INSTALL_CERT_PASSWORD" -A -T "/usr/bin/codesign" -T "/usr/bin/productsign"
-
-# Enable codesigning from a non user interactive shell
-security set-key-partition-list -S apple-tool:,apple:, -s -k $MY_KEYCHAIN_PASSWORD -D "${IDENTITY_CERTIFICATE}" -t private $MY_KEYCHAIN
