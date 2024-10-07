813021e071c9e7d821c2c003640159e700a9c80a
Quincy Cantu <quincyscantu@gmail.com>
2024-10-07 12:35:29 -0600

    Separated arm64 and x64 signing try 7

--- package-resources/mac-signing-arm64.sh
+++ package-resources/mac-signing-arm64.sh
@@ -5,17 +5,16 @@ jar -xf ./jars/cruise-pack-1.0.0-SNAPSHOT-exe.jar BOOT-INF/lib/darklaf-macos-3.0
 jar -xf ./jars/cruise-pack-1.0.0-SNAPSHOT-exe.jar BOOT-INF/lib/sqlite-jdbc-3.45.1.0.jar
 
 echo Extracting dylaf dylib files
-ls
 jar -xf ./BOOT-INF/lib/darklaf-macos-3.0.2.jar com/github/weisj/darklaf/platform/darklaf-macos/libdarklaf-macos-arm64.dylib
 
 echo Extracting sqlite dylib files
 jar -xf ./BOOT-INF/lib/sqlite-jdbc-3.45.1.0.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
 
 echo Signing dylaf dylib files
-/usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.cruisepack.app.' --keychain "$1" --force com/github/weisj/darklaf/platform/darklaf-macos/libdarklaf-macos-arm64.dylib
+/usr/bin/codesign -s "Developer ID Application: University of Colorado Boulder (${2})" -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.cruisepack.app.' --keychain "$1" --force com/github/weisj/darklaf/platform/darklaf-macos/libdarklaf-macos-arm64.dylib
 
 echo Signing sqlite dylib files
-/usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.cruisepack.app.' --keychain "$1" --force org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
+/usr/bin/codesign -s "Developer ID Application: University of Colorado Boulder (${2})" -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.cruisepack.app.' --keychain "$1" --force org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
 
 
 echo Repacking sqlite dylib files
