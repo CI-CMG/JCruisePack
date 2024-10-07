a06994b507b65db1d0d785fdec01ca9a1d7d429c
Quincy Cantu <quincyscantu@gmail.com>
2024-10-07 12:20:20 -0600

    Separated arm64 and x64 signing try 4

--- package-resources/mac-signing-arm64.sh
+++ package-resources/mac-signing-arm64.sh
@@ -5,7 +5,7 @@ jar -xf ./jars/cruise-pack-1.0.0-SNAPSHOT-exe.jar BOOT-INF/lib/darklaf-macos-3.0
 jar -xf ./jars/cruise-pack-1.0.0-SNAPSHOT-exe.jar BOOT-INF/lib/sqlite-jdbc-3.45.1.0.jar
 
 echo Extracting dylaf dylib files
-ls .jars
+ls ./jars
 jar -xf ./jars/BOOT-INF/lib/darklaf-macos-3.0.2.jar com/github/weisj/darklaf/platform/darklaf-macos/libdarklaf-macos-arm64.dylib
 
 echo Extracting sqlite dylib files
