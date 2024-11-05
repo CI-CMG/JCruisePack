b31b399ccd3f4521db9f6e010d64f93a6e5851c5
Quincy Cantu <quincyscantu@gmail.com>
2024-11-05 15:20:00 -0700

    Updated extraction location of jar files during signing

--- package-resources/mac-signing-arm64.sh
+++ package-resources/mac-signing-arm64.sh
@@ -4,11 +4,13 @@ echo Extracting internal jar files
 jar -xf ./jars/cruise-pack-1.0.0-SNAPSHOT-exe.jar BOOT-INF/lib/darklaf-macos-3.0.2.jar
 jar -xf ./jars/cruise-pack-1.0.0-SNAPSHOT-exe.jar BOOT-INF/lib/sqlite-jdbc-3.45.1.0.jar
 
+cd BOOT-INF/lib
+
 echo Extracting dylaf dylib files
-jar -xf ./BOOT-INF/lib/darklaf-macos-3.0.2.jar com/github/weisj/darklaf/platform/darklaf-macos/libdarklaf-macos-arm64.dylib
+jar -xf ./darklaf-macos-3.0.2.jar com/github/weisj/darklaf/platform/darklaf-macos/libdarklaf-macos-arm64.dylib
 
 echo Extracting sqlite dylib files
-jar -xf ./BOOT-INF/lib/sqlite-jdbc-3.45.1.0.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
+jar -xf ./sqlite-jdbc-3.45.1.0.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
 
 echo Signing dylaf dylib files
 /usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.cruisepack.app.' --keychain "$1" --force com/github/weisj/darklaf/platform/darklaf-macos/libdarklaf-macos-arm64.dylib
@@ -18,10 +20,12 @@ echo Signing sqlite dylib files
 
 
 echo Repacking sqlite dylib files
-jar -uf ./BOOT-INF/lib/sqlite-jdbc-3.45.1.0.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
+jar -uf ./sqlite-jdbc-3.45.1.0.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
 
 echo Repacking dylaf dylib files
-jar -uf ./BOOT-INF/lib/darklaf-macos-3.0.2.jar com/github/weisj/darklaf/platform/darklaf-macos/libdarklaf-macos-arm64.dylib
+jar -uf ./darklaf-macos-3.0.2.jar com/github/weisj/darklaf/platform/darklaf-macos/libdarklaf-macos-arm64.dylib
+
+cd ../..
 
 echo Repacking internal jar files
 jar -uf ./jars/cruise-pack-1.0.0-SNAPSHOT-exe.jar BOOT-INF/lib/darklaf-macos-3.0.2.jar
